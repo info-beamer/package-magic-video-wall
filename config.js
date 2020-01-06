c94f8f00bd980fa8abc8f7874185b8b0f95e3ccd
Florian Wesch <fw@dividuum.de>
2020-01-06 14:46:54 +0100

    show realtime mapping preview

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function AprilTags() {
   ])
 
   const detected = Runtime.addFunction(function(
-    id, 
+    id,
     x1,y1,x2,y2,x3,y3,x4,y4,
     m00,m01,m02,m10,m11,m12,m20,m21,m22
   ) {
@@ -22,7 +22,27 @@ function AprilTags() {
     })
   })
 
-  return function(im) {
+  const detect_in_canvas_ctx = function(ctx) {
+    const w = ctx.canvas.width, h = ctx.canvas.height
+    const imageData = ctx.getImageData(0, 0, w, h)
+
+    const buf = Module._malloc(imageData.data.length * imageData.data.BYTES_PER_ELEMENT)
+    Module.HEAPU8.set(imageData.data, buf)
+
+    console.log("Detecting...")
+    tags = []
+    detect(detected, w, h, buf)
+    Module._free(buf)
+
+    console.log("Detected", tags)
+    return {
+      tags: tags,
+      width: w,
+      height: h,
+    }
+  }
+
+  const detect_in_image = function(im) {
     const src_w = im.width
     const src_h = im.height
 
@@ -43,25 +63,14 @@ function AprilTags() {
     canvas.height = h
 
     const ctx = canvas.getContext("2d")
-    ctx.drawImage(im,0,0, w, h)
-
+    ctx.drawImage(im, 0,0, w, h)
     console.log("Fetching scaled image")
-    const imageData = ctx.getImageData(0, 0, w, h)
+    return detect_in_canvas_ctx(ctx)
+  }
 
-    const buf = Module._malloc(imageData.data.length * imageData.data.BYTES_PER_ELEMENT)
-    Module.HEAPU8.set(imageData.data, buf)
-
-    console.log("Detecting...")
-    tags = []
-    detect(detected, w, h, buf)
-    Module._free(buf)
-
-    console.log("Detected", tags)
-    return {
-      tags: tags,
-      width: w,
-      height: h,
-    }
+  return {
+    detect_in_canvas_ctx,
+    detect_in_image,
   }
 }
 
@@ -289,51 +298,59 @@ Vue.component('config-ui', {
           <div class='alert alert-info'>
             <b>Next step</b>: {{message}}
           </div>
-          <div class="btn-group btn-group-justified">
-            <div class="btn-group">
-              <label class="btn btn-primary">
-                <span class='glyphicon glyphicon-upload'></span>
-                Upload/Capture Mapping Picture
-                <input type="file" accept="image/*" @change="onUpload" hidden>
-              </label>
+          <template v-if='is_mapping'>
+            <div class='video'>
+              <video ref='video' autoplay @click="onVideoClick"></video>
+              <canvas ref='preview'></canvas>
             </div>
-            <div class="btn-group">
-              <button
-                class="btn"
-                :class='{
-                  "btn-success": is_capturing,
-                  "btn-primary": !is_capturing,
-                }'
-                :disabled='!can_capture' 
-                @click="onCamButton"
-              >
-                <span class='glyphicon glyphicon-camera'></span>
-                <template v-if='!is_capturing'>
-                  Start Webcam Capture
-                </template>
-                <template v-else>
-                  Capture mapping picture
-                </template>
-              </button>
+            <div class="btn-group btn-group-justified">
+              <div class="btn-group">
+                <button class="btn btn-primary" @click="useMapping"
+                  :disabled='last_detection == null'
+                >
+                  <span class='glyphicon glyphicon-ok'></span>
+                  Use current mapping
+                </button>
+              </div>
+              <div class="btn-group">
+                <button class="btn btn-default" @click="discardMapping">
+                  <span class='glyphicon glyphicon-remove'></span>
+                  Cancel
+                </button>
+              </div>
             </div>
-            <div class="btn-group">
-              <button
-                class='btn btn-primary'
-                :disabled='!has_any_mapping'
-                @click="onResetMapping"
-              >
-                <span class='glyphicon glyphicon-repeat'></span>
-                Reset mapping
-              </button>
+          </template>
+          <template v-else>
+            <div class="btn-group btn-group-justified">
+              <div class="btn-group">
+                <label class="btn btn-primary">
+                  <span class='glyphicon glyphicon-upload'></span>
+                  Upload/Capture Mapping Picture
+                  <input type="file" accept="image/*" @change="onUpload" hidden>
+                </label>
+              </div>
+              <div class="btn-group">
+                <button
+                  class="btn btn-primary"
+                  :disabled='!can_capture'
+                  @click="onCamMapping"
+                >
+                  <span class='glyphicon glyphicon-camera'></span>
+                  Start Webcam Mapping
+                </button>
+              </div>
+              <div class="btn-group">
+                <button
+                  class='btn btn-primary'
+                  :disabled='!has_any_mapping'
+                  @click="onResetMapping"
+                >
+                  <span class='glyphicon glyphicon-repeat'></span>
+                  Reset mapping
+                </button>
+              </div>
             </div>
-          </div>
-          <div class='video' v-if='is_capturing'>
-            <video
-              ref='video'
-              autoplay
-              @click="onVideoClick">
-            </video>
-          </div>
+          </template>
         </div>
       </div>
       <div class='popup'/>
@@ -341,7 +358,9 @@ Vue.component('config-ui', {
   `,
   data: () => ({
     can_capture: !!navigator.mediaDevices,
-    is_capturing: false, 
+    is_mapping: false,
+    preview_timeout: null,
+    last_detection: null,
   }),
   computed: {
     message() {
@@ -377,7 +396,7 @@ Vue.component('config-ui', {
       const im = new Image()
       im.onload = async () => {
         console.log("Got image")
-        const detection = detector(im)
+        const detection = detector.detect_in_image(im)
         const tags = detection.tags
         console.log(tags.length, 'tags detected')
         this.$store.dispatch('config/add_mapping', {
@@ -388,38 +407,94 @@ Vue.component('config-ui', {
       }
       im.src = img_url
     },
-    captureAndCloseVideo() {
+    stopCamMapping() {
+      if (this.preview_timeout) {
+        clearTimeout(this.preview_timeout)
+        this.preview_timeout = null
+      }
       const video = this.$refs.video
-      const width = video.offsetWidth
-      const height = video.offsetHeight
-      const canvas = document.createElement('canvas')
-      canvas.width = width
-      canvas.height = height
-      const context = canvas.getContext('2d')
-      context.drawImage(video, 0, 0, width, height)
       const tracks = video.srcObject.getTracks()
       for (const idx in tracks) {
         tracks[idx].stop()
       }
       video.srcObject = null
-      this.is_capturing = false
-      this.mapFromUrl(canvas.toDataURL('image/png'))
+      this.last_detection = null
+      this.is_mapping = false
     },
-    onCamButton(evt) {
+    useMapping() {
+      const detection = this.last_detection
+      this.$store.dispatch('config/add_mapping', {
+        width: detection.width,
+        height: detection.height,
+        tags: detection.tags,
+      })
+      this.stopCamMapping()
+    },
+    discardMapping() {
+      this.stopCamMapping()
+    },
+    renderPreview() {
+      const video = this.$refs.video
+      const width = video.offsetWidth
+      const height = video.offsetHeight
+
+      const canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(video, 0, 0, width, height)
+
+      const detection = detector.detect_in_canvas_ctx(ctx)
+      const tags = detection.tags
+
+      const preview = this.$refs.preview
+      preview.style.left = video.offsetLeft + 'px'
+      preview.width = width
+      preview.height = height
+      const preview_ctx = preview.getContext('2d')
+      preview_ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      for (const idx in tags) {
+        const tag = tags[idx]
+        const cx = (tag.x1+tag.x2+tag.x3+tag.x4)/4,
+              cy = (tag.y1+tag.y2+tag.y3+tag.y4)/4
+        preview_ctx.lineWidth = 2
+        preview_ctx.fillStyle = 'rgba(0,255,0,0.9)'
+        preview_ctx.beginPath()
+        preview_ctx.moveTo(tag.x1, tag.y1)
+        preview_ctx.lineTo(tag.x2, tag.y2)
+        preview_ctx.lineTo(tag.x3, tag.y3)
+        preview_ctx.lineTo(tag.x4, tag.y4)
+        preview_ctx.closePath()
+        preview_ctx.fill()
+        preview_ctx.fillStyle = 'black'
+        preview_ctx.font = "10px Arial"
+        const screens = this.$store.state.config.screens
+        const screen = screens[(tag.id % 128) - 1]
+        if (screen) {
+          const display = (tag.id > 128) * 1 + 1
+          preview_ctx.fillText(`${screen.serial} / ${display}`, cx-30, cy-2)
+        }
+      }
+      this.last_detection = detection
+      this.preview_timeout = setTimeout(this.renderPreview, 1000)
+    },
+    onCamMapping(evt) {
       evt.target.blur()
-      if (this.is_capturing) {
+      if (this.is_mapping) {
         this.captureAndCloseVideo()
       } else {
         navigator.mediaDevices.getUserMedia({video: true}).then(async stream => {
-          this.is_capturing = true
+          this.is_mapping = true
           await this.$nextTick()
           const video = this.$refs.video
           video.srcObject = stream
-          this.$store.commit('set_message', 'Point the camera to your screens, then click on the capture button or the preview window')
+          this.$store.commit('set_message', 'Point the camera to your screens. Click the "Use current mapping" button to add the detected screens to your video wall.')
         }).catch(err => {
           console.log(err)
           alert("Cannot access the camera")
         })
+        this.preview_timeout = setTimeout(this.renderPreview, 1000)
       }
     },
     onVideoClick() {
